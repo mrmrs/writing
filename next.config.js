dc7719ed752c78f941955d39d92051922829d046
mrmrs <hi@mrmrs.cc>
2019-06-13 16:43:08 -0400

    update

--- next.config.js
+++ next.config.js
@@ -15,5 +15,18 @@ module.exports = withMDX({
   },
   publicRuntimeConfig: {
     linkPrefix: !debug? '/writing' : ''
+  },
+  webpack: (config, { dev }) => {
+    // Perform customizations to webpack config
+    // console.log('webpack');
+    // console.log(config.module.rules, dev);
+    config.module.rules = config.module.rules.map(rule => {
+      if(rule.loader === 'babel-loader') {
+        rule.options.cacheDirectory = false
+      }
+      return rule
+    })
+    // Important: return the modified config
+    return config
   }
 })
